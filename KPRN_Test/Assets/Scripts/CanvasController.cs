4ee03794e140a6025826027863f86b4ed86508fe
IluhaRud <redl2001@outlook.com>
2020-12-24 14:04:00 +0300

    FirstCommit
    commit

---
+++ KPRN_Test/Assets/Scripts/CanvasController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasController : MonoBehaviour
+{
+    public Canvas mainMenu;
+    public Canvas chooseTestMenu;
+
+    public GameObject rockets;
+
+    public void ClickStartButton()
+    {
+        chooseTestMenu.gameObject.SetActive(true);
+        mainMenu.gameObject.SetActive(false);
+    }
+    public void ClickExitButton()
+    {
+        Application.Quit();
+    }
+    public void ClickBackButton()
+    {
+        chooseTestMenu.gameObject.SetActive(false);
+        mainMenu.gameObject.SetActive(true);
+    }
+}
---
+++ KPRN_Test/Assets/Scripts/PossibleAnswerButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PossibleAnswerButton : MonoBehaviour
+{
+    [SerializeField] Button button;
+    [SerializeField] bool isTrue;
+    [SerializeField] bool isClicked;
+
+    [SerializeField] List<Button> buttons;
+
+    private void Awake()
+    {
+        button = gameObject.GetComponent<Button>();
+
+        for (int i = 0; i < 4; i++)
+        {
+            buttons.Add(GameObject.Find($"Variant{i + 1}Button").GetComponent<Button>());
+        }
+    }
+    public void EnableClick()
+    {
+        DisableClick();
+        isClicked = true;
+    }
+    void DisableClick()
+    {
+        for (int i = 0; i < buttons.Count; i++)
+        {
+            buttons[i].GetComponent<PossibleAnswerButton>().isClicked = false;
+        }
+    }
+
+}
